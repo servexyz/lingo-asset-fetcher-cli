4a4708288369ca6fdda118282750b63587092293
alechp <me@alechp.com>
2019-03-19 14:37:46 -0700

    Initialized with react-ink

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-require("dotenv").config();
-console.log(`Hello ${process.env.SAMPLE_ENV}! from node-starter`);
+// require("dotenv").config();
+// console.log(`Hello ${process.env.SAMPLE_ENV}! from node-starter`);
+
+import React from "react";
+import { render, Box, Color } from "ink";
+
+const Demo = () => (
+	<Box>
+		<Color rgb={[255, 255, 255]} bgKeyword="magenta">
+			Hello!
+		</Color>
+		<Color hex="#000000" bgHex="#FFFFFF">
+			Hey there
+		</Color>
+		<Color blue>I'm blue</Color>{" "}
+	</Box>
+);
+render(<Demo />);
