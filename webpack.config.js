1782156fdd4db3abbaa58c3647c9abaa2dc99007
alechp <me@alechp.com>
2019-03-26 14:23:16 -0700

    Fixed regenRuntime issue and added .env.sample

--- webpack.config.js
+++ webpack.config.js
@@ -6,48 +6,48 @@ const NodeExternals = require("webpack-node-externals");
 const SizePlugin = require("size-plugin");
 
 module.exports = {
-  entry: [path.resolve(__dirname, "src/index.js")],
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "main.js",
-    libraryTarget: "commonjs"
-  },
-  target: "node",
-  externals: [NodeExternals()],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            babelrc: true
-          }
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "source-map-loader"
-        }
-      }
-    ]
-  },
-  devtool: "source-map",
-  resolve: {
-    symlinks: false
-  },
-  plugins: [
-    new NodemonPlugin({
-      verbose: false
-    }),
-    new DuplicatePackageCheckerPlugin(),
-    new SizePlugin(),
-    new PacktrackerPlugin({
-      project_token: "a3178144-d3f4-4bd1-97e1-1be10751c52b",
-      upload: true
-    })
-  ]
+	entry: ["@babel/polyfill", path.resolve(__dirname, "src/index.js")],
+	output: {
+		path: path.resolve(__dirname, "build"),
+		filename: "main.js",
+		libraryTarget: "commonjs"
+	},
+	target: "node",
+	externals: [NodeExternals()],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						babelrc: true
+					}
+				}
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "source-map-loader"
+				}
+			}
+		]
+	},
+	devtool: "source-map",
+	resolve: {
+		symlinks: false
+	},
+	plugins: [
+		new NodemonPlugin({
+			verbose: false
+		}),
+		new DuplicatePackageCheckerPlugin(),
+		new SizePlugin(),
+		new PacktrackerPlugin({
+			project_token: "a3178144-d3f4-4bd1-97e1-1be10751c52b",
+			upload: true
+		})
+	]
 };
