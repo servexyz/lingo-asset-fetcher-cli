fa2d91782c0f8839a8224027efe6551234e6aeb0
alechp <me@alechp.com>
2019-04-30 16:00:42 -0700

    Changing test from index to index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,7 +7,9 @@ const npmRun = require("npm-run");
 test("laf fetch", t => {
   t.plan(2);
   let outDir = "./AvaDownloads";
-  let output = npmRun.execSync(`laf fetch -o ${outDir}`);
+  let npx = "npx"; //remote/Travis
+  // let npx = ""; //local/run installed LAF
+  let output = npmRun.execSync(`${npx} laf fetch -o ${outDir}`);
   t.truthy(output);
   let outPath = path.join(__dirname, "../", outDir);
   t.true(fs.existsSync(outPath));
