6e3f9bbcfccdbac6abfdf4d221d2c9e3f6c42a5a
alechp <me@alechp.com>
2019-04-30 16:20:43 -0700

    Added sample .laf.json to make CI testing easier

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,12 +4,16 @@ const path = require("path");
 const fs = require("fs");
 const npmRun = require("npm-run");
 
+let useNpx = true;
 test("laf fetch", t => {
   t.plan(2);
   let outDir = "./AvaDownloads";
-  let npx = "npx"; //remote/Travis
-  // let npx = ""; //local/run installed LAF
-  let output = npmRun.execSync(`${npx} laf fetch -o ${outDir}`);
+  if (useNpx === true) {
+    var npx = "npx"; // used on CI environment
+  } else {
+    npx = ""; //not used when testing local environment / installed binary
+  }
+  let output = npmRun.execSync(`${npx} laf fetch -o '${outDir}'`);
   t.truthy(output);
   let outPath = path.join(__dirname, "../", outDir);
   t.true(fs.existsSync(outPath));
